64d276c2da8ccced983f2de08e0e23ea5f033e61
JuliaShurmei <ivanovaulia428@gmail.com>
2023-06-03 17:42:13 +0300

    Added client

---
+++ TECHLINE/client/src/App.js
@@ -0,0 +1,21 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import ProductsScreen from "./screens/ProductsScreen";
+
+function App() {
+  return (
+    <ChakraProvider>
+      <Router>
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path='/products' element={<ProductsScreen />}></Route>
+          </Routes>
+        </main>
+      </Router>
+    </ChakraProvider>
+  );
+}
+
+export default App;
