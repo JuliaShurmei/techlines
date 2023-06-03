d857a675549e40289de59bfda843d7416a21d228
JuliaShurmei <ivanovaulia428@gmail.com>
2023-06-03 17:11:54 +0300

    Initialized backend

---
+++ TECHLINE/server/database.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const connectToDatabase = async () => {
+  console.log(process.env.MONGO_URI);
+
+  try {
+    mongoose.set("strictQuery", false);
+    const connect = await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+
+    console.log(`MongoDB Connecter: ${connect.connection.host}`);
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+  }
+};
+
+export default connectToDatabase;
