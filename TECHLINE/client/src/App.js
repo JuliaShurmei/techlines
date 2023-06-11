5df19aa6f70ab4b6889cea4d4221bc22f4c75ddb
JuliaShurmei <ivanovaulia428@gmail.com>
2023-06-11 23:04:44 +0300

    Added Registration

--- TECHLINE/client/src/App.js
+++ TECHLINE/client/src/App.js
@@ -7,6 +7,7 @@ import CartScreen from "./screens/CartScreen";
 import Footer from "./components/Footer";
 import LandingScreen from "./screens/LandingScreen";
 import LoginScreen from "./screens/LoginScreen";
+import RegistrationScreen from "./screens/RegistrationScreen";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
             <Route path='/product/:id' element={<ProductScreen />}> </Route>
             <Route path='/cart' element={<CartScreen />}></Route>
             <Route path='/login' element={<LoginScreen />}></Route>
+            <Route path='/registration' element={<RegistrationScreen />}></Route>
           </Routes>
         </main>
         <Footer />
